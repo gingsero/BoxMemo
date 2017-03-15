d63a922cdc41d5e42f028d768e7f25691e55762e
daehui.kim <daehui.kim@systrangroup.com>
2017-03-15 21:10:25 +0900

    Add intro images Update intro activity layout

--- MyApplication/app/src/main/java/demo/systran/com/gitmemo/view/activity/MainActivity.java
+++ MyApplication/app/src/main/java/demo/systran/com/gitmemo/view/activity/MainActivity.java
@@ -16,30 +16,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /* Action bar menu 추가
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId(); // 현재 클릭된 객체의 아이디를 얻어와서
-        // 이하에서 처리
-        switch(id) {
-            case R.id.action_menu:
-                Toast.makeText(getApplicationContext(), "Android Alarm", Toast.LENGTH_SHORT).show();
-                return true;
-
-            case R.id.action_setting:
-                Toast.makeText(getApplicationContext(), "New Game", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-
-    }*/
 }
