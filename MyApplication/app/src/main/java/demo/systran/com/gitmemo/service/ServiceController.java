56adf15e865c3a1d88b4cd61eed934a6b49b2576
DaeHui.KIM <DaeHui.KIM@systrangroup.com>
2017-04-05 21:36:48 +0900

    update import db logic & remove test sqlfile

--- MyApplication/app/src/main/java/demo/systran/com/gitmemo/service/ServiceController.java
+++ MyApplication/app/src/main/java/demo/systran/com/gitmemo/service/ServiceController.java
@@ -66,16 +66,15 @@ public class ServiceController {
 
     public String updateData(){ // eztalky.db 생성 메소드
         String updateResult = null;
-        String dbFilePath = mContext.getFilesDir().getAbsolutePath()+"/eztalky.db";
+        String dbFilePath = mContext.getFilesDir().getAbsolutePath()+"/eztalky.db";// mContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getPath() + "/eztalky.db";
+        Log.d("KKKK", "dbFilePath : " + dbFilePath);
         File dbFile = new File(dbFilePath);
 
         if(!dbFile.isFile()){ // 기존 DB 파일 존재 안 함
             try {
-                Log.d(TAG, "1");
                 InputStream inputStream = mContext.getResources().openRawResource(R.raw.eztalky);//mContext.getAssets().open("eztalky.db"); // Inputstream에 입력할 파일로 res/raw/eztalky.sqlite 넣어줌
-                Log.d(TAG, "2");
                 FileOutputStream fileOutputStream = new FileOutputStream(dbFilePath); // 파일을 생성할 경로를 FileOutputStream에 넣어줌
-                Log.d(TAG, "3");
+
                 byte[] buffer = new byte[4096];
                 while (true) {
                     int readSize = inputStream.read(buffer, 0, buffer.length); // InputStream에 데이터를 읽어서 총 길이 측정후 return/ 그리고 buffer 사이즈에 맞춰서 쪼개 저장
@@ -96,12 +95,10 @@ public class ServiceController {
 
             }
 
-            openHelper = new DBOpenHelper(mContext, "eztalky.db", null, 1); // DB 객체 불러옴 생성
-            mDatabase_ez = openHelper.getReadableDatabase();
 
         }else{ // DB 존재함
             updateResult = "Already exist eztalky.db file";
-            dbFile.delete();//기존 데이터베이스 파일 제거
+//            dbFile.delete();//기존 데이터베이스 파일 제거
         }
 
         return updateResult;
@@ -118,12 +115,25 @@ public class ServiceController {
         Log.d(TAG, "selectDataCount()");
         int count = 0;
 
-        String sql = "SELECT count(*) FROM bestproduct;";
+        String sql = "SELECT count(*) FROM domain";
 
-//        mDatabase = openHelper.getReadableDatabase();
+        openHelper = new DBOpenHelper(mContext, "eztalky.db", null, 1); // DB 객체 불러옴 생성
+        mDatabase_ez = openHelper.getWritableDatabase();
+
         Cursor c = null;
-        c = mDatabase.rawQuery(sql, null);
-        count = c.getCount();
+        if(mDatabase_ez==null){
+            Log.d("KKKK", "mDatabase_ez null");
+            return 0;
+        }
+        try{
+            c = mDatabase_ez.rawQuery(sql, null);
+        } catch (Exception e){
+            Log.d(TAG, e.getMessage());
+        }
+
+        c.moveToFirst();
+//        count = c.getCount();
+        count = c.getInt(0);
         c.close();
         return count;
     }
